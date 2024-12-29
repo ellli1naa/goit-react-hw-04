bea396ba7c9d3d471423f3b040bd0563695ee703
elina <elinaslvv@gmail.com>
2024-12-29 19:54:22 +0200

    fix card

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,14 +2,12 @@ import styles from './ImageCard.module.css';
 
 const ImageCard = ({ alt_description, urls, updateModalStateData }) => {
 	return (
-		<div
-			className={styles.cardWrapper}
-			onClick={() => updateModalStateData(urls.regular, alt_description)}
-		>
+		<div className={styles.cardWrapper}>
 			<img
 				className={styles.cardImage}
 				src={urls.small}
 				alt={alt_description}
+				onClick={() => updateModalStateData(urls.regular, alt_description)}
 			/>
 			<p className={styles.cardDescription}>{alt_description}</p>
 		</div>
